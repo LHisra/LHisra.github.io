53f39cc3fbf3a582e64a0c8b9ab205b960d8b0fe
Israel Silva <jacobb1802@gmail.com>
2018-12-17 16:03:28 -0600

    primer commit

---
+++ precache-manifest.53d7c76694c804faafc66b6beb53fd94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c02ca816efdf669bc1c4",
+    "url": "/static/js/main.c02ca816.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "c02ca816efdf669bc1c4",
+    "url": "/static/css/main.80d5d8b4.chunk.css"
+  },
+  {
+    "revision": "37980bc54e50dfffd809af8ea75f0ed1",
+    "url": "/index.html"
+  }
+];
